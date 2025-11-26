ca6e2cfe0e8467f18632266639040885e627047b
Beca <rehstar12@gmail.com>
2025-11-26 20:27:48 -0300

    correção na seção "keys" do adm

--- src/main/java/com/checkpointgames/app/entity/GameKeys.java
+++ src/main/java/com/checkpointgames/app/entity/GameKeys.java
@@ -1,63 +1,49 @@
 package com.checkpointgames.app.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "gamekeys")
 public class GameKeys {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    
+
     @NotNull
     @ManyToOne(optional = true)
     @JoinColumn(name = "ID_GAME", referencedColumnName = "ID")
     private Games idGame;
-    
+
     @NotBlank
     @Column(name = "KEY", columnDefinition = "TEXT")
     private String key;
-    
+
     @Column(name = "STATUS", columnDefinition = "integer default 0")
-    private Integer status;    
+    private Integer status;
 
-    public Integer getId() {
-        return id;
+    @JsonProperty("gameId")
+    public Integer getGameId() {
+        return idGame != null ? idGame.getId() : null;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    @JsonProperty("gameTitle")
+    public String getGameTitle() {
+        return idGame != null ? idGame.getName() : "Jogo Indefinido";
     }
 
-    public Games getIdGame() {
-        return idGame;
-    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
-    public void setIdGame(Games idGame) {
-        this.idGame = idGame;
-    }
+    public Games getIdGame() { return idGame; }
+    public void setIdGame(Games idGame) { this.idGame = idGame; }
 
-    public String getKey() {
-        return key;
-    }
+    public String getKey() { return key; }
+    public void setKey(String key) { this.key = key; }
 
-    public void setKey(String key) {
-        this.key = key;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-    
-    
-
-    
+    public Integer getStatus() { return status; }
+    public void setStatus(Integer status) { this.status = status; }
 }
